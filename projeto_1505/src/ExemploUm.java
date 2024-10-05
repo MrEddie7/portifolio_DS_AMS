c9438b8b937d8a627500827fa8774085c5558e7f
MrEddie7 <167241764+MrEddie7@users.noreply.github.com>
2024-10-05 16:03:27 -0300

    Update ExemploUm.java

--- projeto_1505/src/ExemploUm.java
+++ projeto_1505/src/ExemploUm.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 import java.util.Scanner;
-/**
- *
- * @author FATEC ZONA LESTE
- */
+
 public class ExemploUm {
     
     public static void main(String[] args) {
