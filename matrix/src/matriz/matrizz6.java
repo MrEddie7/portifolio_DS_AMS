61c1cd4a2d4829485e422c9f125331a2a33a1ad5
Eduardo Cavalcante de Brito <167241764+MrEddie7@users.noreply.github.com>
2024-11-06 14:17:04 -0300

    Atualizar o matriz

---
+++ matrix/src/matriz/matrizz6.java
@@ -0,0 +1,12 @@
+public class MatrizB {
+    public static void main(String[] args) {
+        // Declaração da matriz de inteiros 3x4
+        int[][] matriz = {
+            {19, 25, 100, 99},
+            {10, 7, 25, 14},
+            {35, 2, 47, 74}
+        };
+
+        // Exibe a matriz
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j
