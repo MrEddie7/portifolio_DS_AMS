6f3af38d10ab4b950378bfaa4bd362c133a71be2
Eduardo Cavalcante de Brito <167241764+MrEddie7@users.noreply.github.com>
2024-11-06 14:16:27 -0300

    Atualizar o matriz

---
+++ matrix/src/matriz/matrizz5.java
@@ -0,0 +1,19 @@
+public class MatrizLetras {
+    public static void main(String[] args) {
+        // Declaração da matriz de caracteres 4x5
+        char[][] matriz = {
+            {'a', 'b', 'c', 'd', 'e'},
+            {'f', 'g', 'h', 'i', 'j'},
+            {'l', 'm', 'n', 'o', 'p'},
+            {'q', 'r', 's', 't', 'u'}
+        };
+        
+        // Exibe a matriz
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(); // Quebra de linha após cada linha da matriz
+        }
+    }
+}
